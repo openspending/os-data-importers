aa5dd5007bc4a52ee5205c0559988dd58c092bd7
Adam Kariv <adam.kariv@gmail.com>
2017-05-12 10:48:17 +0300

    adapt to new dpp celery queues

--- initialize.sh
+++ initialize.sh
@@ -17,8 +17,11 @@ pip3 install -U git+git://github.com/frictionlessdata/datapackage-pipelines.git
 #pip3 install -U git+git://github.com/frictionlessdata/tabulator-py.git
 python3 -m common.generate
 cd ..
+rm -f celeryd.pid
+rm -f celerybeat.pid
 dpp init
-dpp
-python3 -m celery -b amqp://guest:guest@mq:5672// --concurrency=4 -B -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker &
+python3 -m celery -b amqp://guest:guest@mq:5672// -A datapackage_pipelines.app -l INFO beat &
+python3 -m celery -b amqp://guest:guest@mq:5672// --concurrency=1 -A datapackage_pipelines.app -Q datapackage-pipelines-management -l INFO worker &
+python3 -m celery -b amqp://guest:guest@mq:5672// --concurrency=4 -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker &
 /usr/bin/env os-types "[]" | true
 dpp serve
