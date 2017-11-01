29ad2af79be665d15a22a155086580d47987d3c8
Vitor Baptista <vitor@vitorbaptista.com>
2017-11-01 16:44:50 +0000

    Print debug msg when waiting for Redis and MQ to start
    Otherwise you won't know why your docker container isn't starting: it's waiting
    for Redis and MQ.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 set -e
 
-while ! ping -c1 redis &>/dev/null; do :; done && echo "REDIS is UP"
-while ! ping -c1 mq &>/dev/null; do :; done && echo "MQ is UP"
+while ! ping -c1 redis &>/dev/null
+do
+  echo "REDIS is DOWN"
+  sleep 1
+done
+echo "REDIS is UP"
+
+while ! ping -c1 mq &>/dev/null
+do
+  echo "MQ is DOWN"
+  sleep 1
+done
+echo "MQ is UP"
 
 cd /app
 
